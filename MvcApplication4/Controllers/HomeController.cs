6dce7736d1e890ab101198adcaf05733eba67236
Steven Fu <sfu@synvata.com>
2013-07-29 08:52:35 +0800

    clear up

--- MvcApplication4/Controllers/HomeController.cs
+++ MvcApplication4/Controllers/HomeController.cs
@@ -6,20 +6,13 @@ using System.Web.Mvc;
 using MvcApplication4.Models;
 namespace MvcApplication4.Controllers
 {
-    public class PersonTest
-    {
-        public String name { get; set; }
-        public int age { get; set; }
-        public bool sex { get; set; }
-        public DateTime birthday { get; set; }
-    }
+   
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.title = "I am index action";
-            var person = new PersonTest{ name = "Michael", age = 30, sex = true, birthday = DateTime.Now };
-            return View(person);
+            
+            return View();
         }
         public ViewResult Contact()
         {
@@ -29,29 +22,7 @@ namespace MvcApplication4.Controllers
         {
             return View("Index");
         }
-        public ViewResult Guest()
-        {
-            ViewBag.Greeting = "Hello, ";
-            return View();
-        }
-        [HttpGet]
-        public ViewResult RsvpForm()
-        {
-            GuestRespone gr = new GuestRespone();   
-            return View(gr);
-        }
-        [HttpPost]
-        public ViewResult RsvpForm(GuestRespone guestResponse)
-        {
-            if (ModelState.IsValid)
-            {
-                return View("Thanks", guestResponse);
-            }
-            else {
-                return View();
-            }
-            
-        }
+        
     }
 
 }
